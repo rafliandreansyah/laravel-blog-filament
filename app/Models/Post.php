67904ba296d1682ab50c34a9b8c25b41076196db
Rafli andreansyah <47078618+rafliandreansyah@users.noreply.github.com>
2024-12-13 23:52:22 +0700

    - add view blogs and integration to database - add pagination on blogs list

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,12 +6,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'slug', 'body', 'thumbnail', 'active', 'user_id', 'published_at'];
+
+    protected $casts = [
+        'published_at' => 'datetime'
+    ];
 
     public function user(): BelongsTo
     {
@@ -21,5 +26,24 @@ class Post extends Model
     public function categories(): BelongsToMany
     {
         return $this->belongsToMany(Category::class);
+    }
+
+    public function shortBody()
+    {
+        return Str::words(strip_tags($this->body));
+    }
+
+    public function published()
+    {
+        return $this->published_at->format('F jS Y');
+    }
+
+    public function getThumbnails()
+    {
+        if (str_starts_with($this->thumbnail, 'http')) {
+            return $this->thumbnail;
+        } else {
+            return '/storage' . $this->thumbnail;
+        }
     }
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <!-- Posts Section -->
+    <section class="w-full md:w-2/3 flex flex-col items-center px-3">
+
+        @foreach ($posts as $post)
+            <x-post-item :post="$post"></x-post-item>
+        @endforeach
+
+        {{ $posts->links() }}
+
+    </section>
+
+</x-app-layout>
